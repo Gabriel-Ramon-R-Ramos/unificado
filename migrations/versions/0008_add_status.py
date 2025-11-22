d4041ad8a1b2db85c5839e1dfa92115183da79bd
Gabriel Ramon <garamon97@gmail.com>
2025-11-21 22:16:48 -0300

    fix: error migrations

--- migrations/versions/0008_add_status.py
+++ migrations/versions/0008_add_status.py
@@ -1,6 +1,6 @@
 """add status to students_disciplines
 
-Revision ID: 0008_add_status_to_students_disciplines
+Revision ID: 0008_add_status
 Revises: 0007_add_ads_course
 Create Date: 2025-11-21 01:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '0008_add_status_to_students_disciplines'
+revision: str = '0008_add_status'
 down_revision: Union[str, Sequence[str], None] = '0007_add_ads_course'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
